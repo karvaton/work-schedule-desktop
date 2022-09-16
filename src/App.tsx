046e4455602167884d6816d72e398e725c337ec2
Karvaton <karvaton220@gmail.com>
2022-09-16 22:05:38 +0300

    Adde menu and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Calendar from './Components/Calendar';
+import Calendar from './Components/Calendar/Calendar';
+import Menu from './Components/Menu';
 import './style/App.css';
 
 
 function App() {
     return (
         <div className="App">
+            <aside>
+                <Menu />
+            </aside>
             <main>
                 <Calendar />
             </main>
