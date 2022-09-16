5eb1cee565167568b482f2ecc9f26b6ab6d2393c
Karvaton <karvaton220@gmail.com>
2022-09-16 20:50:14 +0300

    Added menu and styles

---
+++ src/Components/Menu.tsx
@@ -0,0 +1,12 @@
+import '../style/menu.css';
+
+
+export default function Menu() {
+    return (
+        <ul id="menu" className="menu">
+            <li className="add-schedule schedules">
+                Add schedule +
+            </li>
+        </ul>
+    )
+}
