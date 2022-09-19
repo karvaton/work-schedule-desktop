5775132a293b16fca5d42622a3dae826fdb4b8b4
Karvaton <karvaton220@gmail.com>
2022-09-19 16:17:29 +0300

    Updated model

--- src/models/iSchedules.ts
+++ src/models/iSchedules.ts
@@ -6,7 +6,7 @@ export interface iSchedule {
     firstDate: iDate
     countOfWorkdays: number
     countOfWeekends: number
-    // exeptions: iDate[]
+    exeptions?: iDate[]
 }
 
 export interface iSchedules {
